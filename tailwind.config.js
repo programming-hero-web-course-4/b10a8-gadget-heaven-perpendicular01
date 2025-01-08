70909e69f06fbda09ae9c8e568890fdaaf4b7c6e
saif <kh.s.k.rohan@gmail.com>
2025-01-08 14:15:26 +0600

    setup done

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sora: ['Sora', 'sans-serif'],
+      },
+      colors:{
+        
+      },
+    },
+  },
+  plugins: [
+    require('daisyui'),
+  ],
+}
+
